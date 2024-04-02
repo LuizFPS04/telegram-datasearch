15914204b88d80662aef4c366751d8540e7c61d4
Aluno <Aluno@LAB202-PC09.ifmg.edu.br>
2024-04-02 09:31:38 -0300

    feat: adding useful documents and creating the project's main class

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Telegram Dataset Project");
+
+    }
+}
