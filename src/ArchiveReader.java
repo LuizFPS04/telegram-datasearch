50fad3f4b22fb89f44c3d33b26e319c83bbf5313
Rafi Gabriel <rafigabriel.32@gmail.com>
2024-04-03 20:40:26 -0300

    Começo do escopo de como será armazenado os campos dentro dos vetores

--- src/ArchiveReader.java
+++ src/ArchiveReader.java
@@ -2,19 +2,35 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 
-public class ArchiveReader { 
+public class ArchiveReader {
     public static void ReadFile() {
         try {
-            BufferedReader bf = new BufferedReader(new FileReader("C:/Users/grafi/Desktop/Telegram Data/telegram-datasearch/docs/telegram_corpus.txt"));
-            String line =  bf.readLine();
-            while(line != null){
-                line = bf.readLine();
-                String [] fields = line.split(":");
-                System.out.println(line);
+            BufferedReader bf = new BufferedReader(new FileReader(
+                    "C:/Users/grafi/Desktop/Telegram Data/telegram-datasearch/docs/telegram_corpus.txt"));
+            String line = bf.readLine();
+            int count = 0;
+            while (line != null) {
+                if (line == "{" || line == "},") {
+                    line = bf.readLine();
+                } else {
+                    String[] fields = line.split("\": ");
+                    // 0 = date 1 = numero 2 = messae 3 = numero
+                    for (String string : fields) {
+                        System.out.println(string);
+                    }
+
+                    count++;
+                    if (count == 8) {
+                        // 1
+
+                        // reseto array
+                    }
+                }
+
             }
         } catch (Exception e) {
             e.printStackTrace();
-            
+
         }
     }
- }
+}
