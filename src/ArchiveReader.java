899500a65ebd9aec93ce1ac99e0fc3564f9e067d
Luiz Felipe <luizfpsena04@gmail.com>
2024-04-10 23:36:24 -0300

    fix: added javadoc and corrected return of message fields

--- src/ArchiveReader.java
+++ src/ArchiveReader.java
@@ -19,27 +19,23 @@ public class ArchiveReader {
                         line = bf.readLine();
                     } else {
                         line = line.replaceAll("\"", "");
-                        String[] momentaneo = line.split(": ");
-                        fields[index] = momentaneo[1];
+                        String[] moment = line.split(": ");
+                        fields[index] = moment[1];
 
                         index++;
                         if (index == 8) {
-                            Messages messages = new Messages(fields[0], fields[0], fields[0], fields[0], fields[0],
-                                    fields[0], fields[0], fields[0]);
+                            Messages messages = new Messages(fields[0], fields[1], fields[2], fields[3], fields[4],
+                                    fields[5], fields[6], fields[7]);
                             messages.showAll();
                             index = 0;
                             System.out.println("==============================");
                         }
                         line = bf.readLine();
                     }
-
                 }
-
             }
-
         } catch (Exception e) {
             e.printStackTrace();
-
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class Main {
+    
+    /** 
+     * @param args
+     */
     public static void main(String[] args) {
-
         System.out.println("Telegram Dataset Project");
         ArchiveReader.ReadFile();
-
-       
-     
     }
 }
